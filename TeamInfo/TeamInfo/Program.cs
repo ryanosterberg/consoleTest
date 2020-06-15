6edc4cfa5bdab7044e69366d7e4dca8734c68f06
Mr O <ryan.p.osterberg@gmail.com>
2020-06-15 07:31:53 -0500

    Hello World

---
+++ TeamInfo/TeamInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamInfo
+{
+    class Program
+    {
+        //Part 1: add your name, what school you will attend next year, and the answer to the question 
+        //“What is the worst gift you have received?”
+
+        //Part 2: add another answer to the question “What I want for graduation?” at some random point in the project
+        //And Delete a part of another teammate's code
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
